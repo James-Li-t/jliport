2061192d28a819935a3813a3e66ea9b777854465
Jamesli28000@gmail.com <41440719+James-Li-t@users.noreply.github.com>
2022-07-10 00:20:35 -0400

    basic homepage

--- pages/index.js
+++ pages/index.js
@@ -5,30 +5,31 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>James Liのホムペーじ</title>
-        <meta name="jliport" content="All about James Li" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Head>
+    <title>James Liのホムペーじ</title>
+    <meta name="jliport" content="All about James Li" />
+    <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main className={styles.main}>
-        
-        <h1 className={styles.title}>
-        THIS IS A PLACEHOLDER PAGE
-        </h1>
-        
-        <h4 className={styles.title}>
-        and then I went..
-        <a href="https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A/featured">WHAT!
-        </a>
-        </h4>
-    
-        <h1 className='m-8 text-white text-5xl underlinetext-3xl font-bold'>
-        星街すいせい
-        </h1>
+    <main className={styles.main}>
 
+    <div className='my-10'>
+    <h1 className={styles.title}>
+    JAMES LI 
+    </h1>
+    <h1 className = {styles.description}>
+    welcome to my homepage
+    </h1>
 
-      </main>
+    </div>
+    <p className='mx-8 text-gray-300 font-mono'>
+    Hey! I'm James Li, aspiring heroin conoissuer and Valorant extrodinaire 😛. You can find a list
+    of all the substances I abuse <a href="https://en.wikipedia.org/wiki/Drugs_controlled_by_the_UK_Misuse_of_Drugs_Act"> here. </a>
+    </p>
+    <div>
+
+    </div>
+    </main>
     </div>
   )
 }
